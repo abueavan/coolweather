c89299e5afb7af7d51ec992db316fdf257783f1f
abu <1358950191@qq.com>
2018-07-19 16:58:39 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,26 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Eavan on 2018/7/19.
+ */
+
+public class Basic {
+
+    @SerializedName("city")
+    public String cityName;
+
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update {
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,24 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Eavan on 2018/7/19.
+ */
+
+public class Now {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
+
+}
